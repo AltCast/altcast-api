464e9ef32718c18e82c844cb14b8749f09986743
André Abadesso <andre@lab21k.com.br>
2017-12-16 16:48:06 -0200

    feat: improved logging

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 let WebSocketServer = require('uws').Server
 let wss = new WebSocketServer({ port: 1337 })
 let redisClient = require('redis').createClient({
-    host: process.env.REDIS_HOST,
-    port: process.env_REDIS_PORT
+    host: process.env.REDIS_HOST || 'localhost',
+    port: process.env_REDIS_PORT || 6379
 })
 
 let channels = {}
 let redisSubscribedChannels = []
 
 let wsOnSubscribe = (channel, socket) => {
+    console.log('Client subscribed to', channel)
+
     if (redisSubscribedChannels.indexOf(channel) < 0) {
         redisClient.subscribe(channel)
     }
@@ -28,12 +30,16 @@ let wsOnMessage = (payload, socket) => {
         console.debug('Ignoring payload: ', payload)
     }
 }
+
+let redisOnMessage = (channel, message) => {
+    console.log(`Received message from redis sub: ${channel}: ${message}`)
+
+    if (channels.hasOwnProperty(channel))
+        channels[channel].forEach((ws) => ws.send(message))
+}
+
+redisClient.on('message', (channel, message) => redisOnMessage(channel, message))
  
 wss.on('connection', (ws) => {
     ws.on('message', (payload) => wsOnMessage(payload, ws))
 })
-
-let redisOnMessage = (channel, message) => {
-    if (channels.hasOwnProperty(channel))
-        channels[channel].forEach((ws) => ws.send(message))
-}
